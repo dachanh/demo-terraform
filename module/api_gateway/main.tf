a1fedac59e9a3630eb3cb9bd52ed8c53da975e3d
dachanh <ghostcodert7@gmail.com>
2023-11-20 13:52:01 +0700

    format

--- module/api_gateway/main.tf
+++ module/api_gateway/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_internet_gateway" "vpc" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id = var.vpc_id
   tags = {
     Name        = "${var.vpc_name}-${var.environment}"
     environment = var.environment
--- module/api_gateway/variables.tf
+++ module/api_gateway/variables.tf
@@ -3,3 +3,11 @@
 variable "vpc_id" {
     type = string
 }
+
+variable "vpc_name" {
+    type = string
+}
+
+variable "environment" {
+    type =  string
+}
